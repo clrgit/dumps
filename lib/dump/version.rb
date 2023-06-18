447b19d4897df4b8c354f3f2bc25bd5d8ce23fbc
Claus Rasmussen <claus.l.rasmussen@gmail.com>
2023-06-18 08:47:14 +0200

    Initial setup

---
+++ lib/dump/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Dump
+  VERSION = "0.0.0"
+end
---
+++ spec/dump_spec.rb
@@ -0,0 +1,7 @@
+describe Dump do
+  it 'has a version number' do
+    expect(Dump::VERSION).not_to be_nil
+  end
+
+  it 'does something useful'
+end
